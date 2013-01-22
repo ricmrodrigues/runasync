7e58a7f94bc28b59ffc3692227a215fc89b70cac
ricmrodrigues <ricmrodrigues@gmail.com>
2013-01-22 22:22:51 +0000

    first commit

---
+++ runasync.js
@@ -0,0 +1,45 @@
+/*
+ * RunAsync v0.1
+ * http://github.com/ricmrodrigues/runasync
+ *
+ * Library that allows you to execute JavaScript asynchronously
+ * seamlessly using modern browser capabitilies
+ *
+ * Copyright 2013 @ricmrodrigues
+ * Released under the MIT license
+ * http://mit-license.org/
+ *
+ */
+var RunAsync = (function() {
+	"use strict";
+	var BlobBuilder = BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder ||
+                     window.MozBlobBuilder || window.MSBlobBuilder,
+        url = window.URL || window.webkitURL;
+    
+    if (!BlobBuilder && !Blob) {
+        throw "This browser does not support DoAsync";
+    }
+
+    return function(task, params) {
+        var blob = null,
+            promise = $.Deferred(),
+            func = "onmessage = function(e) { var taskResult = ("+task.toString()+")(e.data); postMessage(taskResult); }";     
+        
+        if (Blob) {
+            blob = new Blob([func], {type: 'text/javascript'});
+        }
+        else {
+            var bb = new BlobBuilder();
+            bb.append(func);                    
+            blob = bb.getBlob("text/javascript");
+        }
+        
+        var blobUrl = url.createObjectURL(blob),        
+        	worker = new Worker(blobUrl);
+        worker.onmessage = function(e) {
+            promise.resolve(e.data);
+        };
+        worker.postMessage(params); // Start the worker.    
+        return promise;
+    };
+})();
