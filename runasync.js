03fbe8d68958a7481918d8de59485e3a30631f94
ricmrodrigues <ricmrodrigues@gmail.com>
2013-01-23 13:50:12 +0000

    changed to Task.Run( () => {} [, parameters ])

--- runasync.js
+++ runasync.js
@@ -10,36 +10,38 @@
  * http://mit-license.org/
  *
  */
-var RunAsync = (function() {
+var Task = (function() {
 	"use strict";
 	var BlobBuilder = BlobBuilder = window.BlobBuilder || window.WebKitBlobBuilder ||
                      window.MozBlobBuilder || window.MSBlobBuilder,
         url = window.URL || window.webkitURL;
     
     if (!BlobBuilder && !Blob) {
-        throw "This browser does not support RunAsync";
+        throw "This browser does not support Task.Run";
     }
 
-    return function(task, params) {
-        var blob = null,
-            promise = $.Deferred(),
-            func = "onmessage = function(e) { var taskResult = ("+task.toString()+")(e.data); postMessage(taskResult); }";     
-        
-        if (Blob) {
-            blob = new Blob([func], {type: 'text/javascript'});
-        }
-        else {
-            var bb = new BlobBuilder();
-            bb.append(func);                    
-            blob = bb.getBlob("text/javascript");
-        }
-        
-        var blobUrl = url.createObjectURL(blob),        
-        	worker = new Worker(blobUrl);
-        worker.onmessage = function(e) {
-            promise.resolve(e.data);
-        };
-        worker.postMessage(params); // Start the worker.    
-        return promise;
-    };
+    return {
+    	Run: function(task, params) {
+			var blob = null,
+			    promise = $.Deferred(),
+			    func = "onmessage = function(e) { var taskResult = ("+task.toString()+")(e.data); postMessage(taskResult); }";     
+			
+			if (Blob) {
+			    blob = new Blob([func], {type: 'text/javascript'});
+			}
+			else {
+			    var bb = new BlobBuilder();
+			    bb.append(func);                    
+			    blob = bb.getBlob("text/javascript");
+			}
+			
+			var blobUrl = url.createObjectURL(blob),        
+				worker = new Worker(blobUrl);
+			worker.onmessage = function(e) {
+			    promise.resolve(e.data);
+			};
+			worker.postMessage(params); // Start the worker.    
+			return promise;
+		};
+    };    	
 })();
