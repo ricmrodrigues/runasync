c1493b4cad3e1bcd5f1d930d17624188ea2e5e00
ricmrodrigues <ricmrodrigues@gmail.com>
2013-01-28 11:39:26 +0000

    removed stupid comment

--- grunt.js
+++ grunt.js
@@ -60,5 +60,5 @@ module.exports = function (grunt) {
     grunt.registerTask('default', 'lint qunit concat min');
 
     // Travis CI task.
-    grunt.registerTask('travis', 'lint qunit'); //removed qunit
+    grunt.registerTask('travis', 'lint qunit');
 };
